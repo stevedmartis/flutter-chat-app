682a32043cb40aa1847308d14db1064595d9c599
DavidSteveMars <demaromail@gmail.com>
2020-12-26 20:55:32 -0300

    url socket ok

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -4,8 +4,9 @@ class Environment {
   //static String apiUrl    = Platform.isAndroid ? 'http://10.0.2.2:3000/api' : 'http://localhost:3000/api';
 
   // 'http://ec2-54-207-202-146.sa-east-1.compute.amazonaws.com:3000/api';
-  static String apiUrl = 'http://localhost:3000/api';
+  static String apiUrl =
+      'http://ec2-54-207-202-146.sa-east-1.compute.amazonaws.com:3000/api';
 
   static String socketUrl =
-      Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
+      'http://ec2-54-207-202-146.sa-east-1.compute.amazonaws.com:3000';
 }
